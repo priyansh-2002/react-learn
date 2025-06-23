bbdc6e03cb4712e9b70b567e98e48f1926925ac8
priyansh-2002 <priyansh.hss02@gmail.com>
2025-06-23 20:19:38 +0530

    basic react and vite

---
+++ 01_basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Coffee and react</h1>
+  );
+}
+
+export default App;
---
+++ 01_vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  
+
+  return (
+    <h1>Coffee and react with vite | Priyansh</h1>
+  )
+}
+
+export default App
